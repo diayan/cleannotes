b13155db956a01c93e079f873082da8a6e849009
diayan <diayans@outlook.com>
2023-01-19 13:24:46 +0000

    Initial Commit

---
+++ CleanNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanNotes
+//
+//  Created by diayan siat on 19/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
