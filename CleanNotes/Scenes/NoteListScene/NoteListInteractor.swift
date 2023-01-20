b979e8499408b05101094a64d6ed6605a298bf12
diayan <diayans@outlook.com>
2023-01-20 11:02:18 +0000

    create boiler plates for note list scene

---
+++ CleanNotes/Scenes/NoteListScene/NoteListInteractor.swift
@@ -0,0 +1,8 @@
+//
+//  NoteListInteractor.swift
+//  CleanNotes
+//
+//  Created by diayan siat on 19/01/2023.
+//
+
+import Foundation
--- CleanNotes/Scenes/NoteListScene/NoteListVC.swift
+++ CleanNotes/Scenes/NoteListScene/NoteListVC.swift
@@ -9,9 +9,55 @@ import UIKit
 
 class NoteListVC: UIViewController {
 
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.dequeueReusableCell(withIdentifier: NoteListTableViewCell.identifier)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    lazy var floatingButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        print("class initialized")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        layoutViews()
         view.backgroundColor = .systemPink
+    }
+    
+    func layoutViews() {
+        view.addSubview(tableView)
+        view.addSubview(floatingButton)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
+
+extension NoteListVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: NoteListTableViewCell.identifier, for: indexPath)
+        return cell
     }
 }
 
